e7a1d38a65cce9388151b811427bca296da27849
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-09-11 21:22:06 +0300

    A class initializing the database with initial data was created.

--- Entities/Users/User.cs
+++ Entities/Users/User.cs
@@ -13,6 +13,11 @@ namespace Library.Domain.Entities.Users
     public class User : UserBase
     {
         public ICollection<Request> Requests { get; set; } = new List<Request>();
+
+        public User(string email, string firstName, string lastName, string? patronymic = null)
+            : base(email, firstName, lastName, patronymic)
+        {
+        }
     }
 
 }
